900082e2eccabdd7984393a5973e149192c5d056
devconcept <devconcept@outlook.com>
2018-02-15 16:02:19 -0500

    Added AddToCart component

---
+++ src/components/add-to-cart/add-to-cart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-to-cart-demo',
+  templateUrl: './add-to-cart.component.html',
+  styleUrls: ['./add-to-cart.component.scss']
+})
+export class AddToCartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
