3847d14244f529e0ff55c7f7caa36ed7a8c9b761
devconcept <devconcept@outlook.com>
2018-03-02 13:53:59 -0500

    Added quantity buttons

--- src/components/cart-list-layout/cart-list-layout.component.ts
+++ src/components/cart-list-layout/cart-list-layout.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CartItem } from '../../classes/cart-item';
-import { CartViewDisplay } from '../../types';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'cart-list-layout',
@@ -8,4 +8,20 @@ import { CartViewDisplay } from '../../types';
 })
 export class CartListLayoutComponent {
   @Input() items: CartItem[];
+
+  constructor(private cartService: CartService) {
+
+  }
+
+  increase(item: CartItem) {
+    item.quantity++;
+    this.cartService.addItem(item.id, item.name, item.price, item.quantity, item.data);
+  }
+
+  decrease(item: CartItem) {
+    if (item.quantity > 1) {
+      item.quantity--;
+    }
+    this.cartService.addItem(item.id, item.name, item.price, item.quantity, item.data);
+  }
 }
