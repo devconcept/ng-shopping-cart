28616a9559f113755a0db7c0305ee30a01122e8f
devconcept <devconcept@outlook.com>
2018-03-13 08:47:20 -0400

    Fixed demo checkout test

--- demo/app/cart-checkout-demo/cart-checkout-demo.component.spec.ts
+++ demo/app/cart-checkout-demo/cart-checkout-demo.component.spec.ts
@@ -2,19 +2,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CartCheckoutDemoComponent } from './cart-checkout-demo.component';
 import { CartCheckoutComponent, CartService, MemoryCartService } from '../../../src';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CartCheckoutDemoComponent', () => {
   let component: CartCheckoutDemoComponent;
   let fixture: ComponentFixture<CartCheckoutDemoComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CartCheckoutDemoComponent, CartCheckoutComponent ],
-      providers: [
-        { provide: CartService, useClass: MemoryCartService }
-      ]
-    })
-    .compileComponents();
+    TestBed
+      .configureTestingModule({
+        declarations: [CartCheckoutDemoComponent, CartCheckoutComponent],
+        imports: [HttpClientModule],
+        providers: [
+          { provide: CartService, useClass: MemoryCartService }
+        ]
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
