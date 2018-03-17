61124842a1e602af9a79d107c1b9fdb9ff6e5baf
devconcept <devconcept@outlook.com>
2018-03-17 11:53:48 -0400

    Changed default type for CartView

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,6 @@ import { CheckoutHttpSettings } from './interfaces/checkout-http-settings';
 
 export type AddToCartType = 'button' | 'dropdown' | 'text' | 'number';
 export type AddToCartPosition = 'top' | 'bottom' | 'left' | 'right';
-export type CartViewDisplay = 'responsive' | 'adaptative' | 'fixed';
+export type CartViewDisplay = 'responsive' | 'responsive-table' | 'fixed';
 export type CheckoutType = 'log' | 'http' | 'paypal';
 export type CheckoutSettings = null | CheckoutPaypalSettings | CheckoutHttpSettings;
