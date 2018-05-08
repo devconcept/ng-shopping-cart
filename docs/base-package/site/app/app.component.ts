e44fdc4911c5ed629bbe71eb17b8bc5f0facdb77
devconcept <devconcept@outlook.com>
2018-05-08 12:01:09 -0500

    Fixed footer position on small devices

--- docs/base-package/site/app/app.component.ts
+++ docs/base-package/site/app/app.component.ts
@@ -11,6 +11,7 @@ import { InfoService } from './shared/services/info-service';
 })
 export class AppComponent implements OnInit {
   github: SafeHtml;
+  law: SafeHtml;
   license: string;
 
   constructor(private sanitizer: DomSanitizer, private infoService: InfoService) {
@@ -19,6 +20,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.github = this.sanitizer.bypassSecurityTrustHtml(octicons.octoface.toSVG({style: 'vertical-align: middle'}));
+    this.law = this.sanitizer.bypassSecurityTrustHtml(octicons.law.toSVG({style: 'vertical-align: text-bottom'}));
     this.license = this.infoService.getRepoUrl() + '/blob/master/LICENSE';
   }
 }
