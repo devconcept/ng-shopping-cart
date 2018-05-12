7ce977970a6acce7ed2f4ab3ceaf0f785d204bfe
devconcept <devconcept@outlook.com>
2018-05-12 17:08:58 -0500

    Removed disabled class for projected content

--- demo/app/demo-cart-checkout/demo-cart-checkout.component.ts
+++ demo/app/demo-cart-checkout/demo-cart-checkout.component.ts
@@ -1,13 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit} from '@angular/core';
+import {CartService} from '../../../src/classes/cart.service';
+import {DemoCartItem} from '../demo-cart-item';
 
 @Component({
   selector: 'demo-cart-checkout',
   templateUrl: './demo-cart-checkout.component.html',
   styleUrls: ['./demo-cart-checkout.component.scss'],
 })
-export class DemoCheckoutComponent  {
+export class DemoCheckoutComponent implements OnInit, OnDestroy {
+  private serviceSubstription: EventEmitter<any>;
   custom = false;
   label = 'Checkout';
   settingsCollapsed = false;
   resultsCollapsed = false;
+  disabled = false;
+
+  constructor(private cartService: CartService<DemoCartItem>) {
+
+  }
+
+  ngOnInit(): void {
+    this.checkService();
+    this.serviceSubstription = this.cartService.onItemsChanged.subscribe(() => {
+      this.checkService();
+    });
+  }
+
+  checkService() {
+    this.disabled = this.cartService.isEmpty();
+  }
+
+  ngOnDestroy(): void {
+    this.serviceSubstription.unsubscribe();
+  }
+
 }
