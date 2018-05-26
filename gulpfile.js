540b69b891668040086c016ef6c9300cb187ab4c
devconcept <devconcept@outlook.com>
2018-05-25 22:41:32 -0500

    Automated docs generation and compilation

--- gulpfile.js
+++ gulpfile.js
@@ -7,51 +7,71 @@ const cleanCss = require('gulp-clean-css');
 const pify = require('pify');
 const git = pify(require('gulp-git'));
 const pkg = require('./package.json');
+const {exec} = require('child_process');
+const pump = require('pump');
 
 gulp.task('lib:clean', () => {
   return del('./dist/styles');
 });
 
-gulp.task('lib:sass', ['lib:clean'], () => {
-  return gulp.src('./src/styles/sass/*.*')
-    .pipe(gulp.dest('./dist/styles/sass'))
+gulp.task('lib:sass', ['lib:clean'], (cb) => {
+  pump([
+    gulp.src('./src/styles/sass/*.*'),
+    gulp.dest('./dist/styles/sass'),
+  ], cb);
 });
 
-gulp.task('lib:css:compile', ['lib:clean'], () => {
-  return gulp.src('./src/styles/sass/index.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(rename('ng-shopping-cart.css'))
-    .pipe(gulp.dest('./dist/styles/css'))
+gulp.task('lib:css:compile', ['lib:clean'], (cb) => {
+  pump([
+    gulp.src('./src/styles/sass/index.scss'),
+    sass(),
+    rename('ng-shopping-cart.css'),
+    gulp.dest('./dist/styles/css'),
+  ], cb);
 });
 
-gulp.task('lib:css:min', ['lib:css:compile'], () => {
-  return gulp.src('./dist/styles/css/ng-shopping-cart.css')
-    .pipe(rename('ng-shopping-cart.min.css'))
-    .pipe(sourceMaps.init())
-    .pipe(cleanCss({compatibility: 'ie10'}))
-    .pipe(sourceMaps.write('.'))
-    .pipe(gulp.dest('./dist/styles/css'))
+gulp.task('lib:css:min', ['lib:css:compile'], (cb) => {
+  pump([
+    gulp.src('./dist/styles/css/ng-shopping-cart.css'),
+    rename('ng-shopping-cart.min.css'),
+    sourceMaps.init(),
+    cleanCss({compatibility: 'ie10'}),
+    sourceMaps.write('.'),
+    gulp.dest('./dist/styles/css'),
+  ], cb);
 });
 
 gulp.task('lib:css', ['lib:css:min']);
 
-gulp.task('docs:checkout', () => {
+gulp.task('docs:generate', (cb) => {
+  exec('npm run docs', {windowsHide: true}, cb);
+});
+
+gulp.task('docs:compile', ['docs:generate'], (cb) => {
+  exec('npm run docs:build', {windowsHide: true}, cb);
+});
+
+gulp.task('docs:checkout', ['docs:compile'], () => {
   return git.checkout('gh-pages');
 });
 
 gulp.task('docs:clean', ['docs:checkout'], () => {
-  return del(['*.js', '*.css', '3rdpartylicenses.txt', '*.html', './assets/*']);
+  return del(['*.js', '*.css', '*.html', './assets/*', '3rdpartylicenses.txt', '.nojekyll', 'favicon.ico']);
 });
 
 gulp.task('docs:update', ['docs:clean'], () => {
-  return gulp.src('./docs-dist')
-    .pipe(gulp.dest('./'));
+  pump([
+    gulp.src('./docs-dist/*'),
+    gulp.dest('./')
+  ], cb);
 });
 
 gulp.task('docs:index', ['docs:update'], () => {
-  return gulp.src('./index.html')
-    .pipe(rename('404.html'))
-    .pipe(gulp.dest('./'));
+  pump([
+    gulp.src('./index.html'),
+    rename('404.html'),
+    gulp.dest('./'),
+  ], cb);
 });
 
 gulp.task('docs:commit', ['docs:index'], () => {
@@ -63,7 +83,8 @@ gulp.task('docs:finish', ['docs:commit'], () => {
   return git.checkout('develop');
 });
 
-gulp.task('docs', ['docs:finish']);
+//gulp.task('docs', ['docs:finish']);
+gulp.task('docs', ['docs:compile']);
 
 gulp.task('lib', ['lib:sass', 'lib:css']);
 
