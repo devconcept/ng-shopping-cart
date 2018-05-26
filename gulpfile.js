1a8e491301ad3c8ce3ebbaa0272df387b86e442c
devconcept <devconcept@outlook.com>
2018-05-25 21:47:32 -0500

    Automated docs generation

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const sourceMaps = require('gulp-sourcemaps');
 const cleanCss = require('gulp-clean-css');
+const pify = require('pify');
+const git = pify(require('gulp-git'));
+const pkg = require('./package.json');
 
 gulp.task('lib:clean', () => {
   return del('./dist/styles');
@@ -32,9 +35,35 @@ gulp.task('lib:css:min', ['lib:css:compile'], () => {
 
 gulp.task('lib:css', ['lib:css:min']);
 
-gulp.task('docs:clean', () => {
-  return del('./docs-build');
+gulp.task('docs:clean', ['docs:checkout'], () => {
+  return del(['*.js', '*.css', '3rdpartylicenses.txt', '*.html', './assets/*']);
 });
+
+gulp.task('docs:checkout', () => {
+  return git.checkout('gh-pages');
+});
+
+gulp.task('docs:update', ['docs:clean'], () => {
+  return gulp.src('./docs-dist')
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('docs:index', ['docs:update'], () => {
+  return gulp.src('./index.html')
+    .pipe(rename('404.html'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('docs:commit', () => {
+  return git.commit(`Updating docs version ${pkg.version} at ${new Date().toISOString()}`);
+});
+
+
+gulp.task('docs:finish', ['docs:commit'], () => {
+  return git.checkout('develop');
+});
+
+gulp.task('docs', ['docs:finish']);
 
 gulp.task('lib', ['lib:sass', 'lib:css']);
 
