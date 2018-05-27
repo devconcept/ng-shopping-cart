5291ba707d17b6d57b7d92ac119f232724fac2ce
devconcept <devconcept@outlook.com>
2018-05-27 08:41:34 -0500

    Added i18n interface

--- src/components/cart-summary/cart-summary.component.ts
+++ src/components/cart-summary/cart-summary.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { CartService } from '../../classes/cart.service';
+import {I18nPlurals} from '../../interfaces/i18n-plurals';
 
 /**
  * Renders a summary of the contents of the cart.
@@ -34,7 +35,7 @@ export class CartSummaryComponent implements OnInit, OnDestroy {
    * The component uses the i18nPlural pipe to translate the number of items of the cart according to locale rules using the ICU format.
    * You can use this binding to internationalize you app or to change how values are converted into words.
    */
-  @Input() totalPlurals: { [k: string]: string } = { '=0': 'No items', '=1': 'One item', 'other': '# items' };
+  @Input() totalPlurals: I18nPlurals = { '=0': 'No items', '=1': 'One item', 'other': '# items' };
   totalItems = 0;
   totalCost = 0;
 
---
+++ src/interfaces/i18n-plurals.ts
@@ -0,0 +1,6 @@
+/**
+ * This is the format used by the i18nPlural pipe to map values from values to words. Check the Angular docs for more info.
+ */
+export interface I18nPlurals {
+  [k: string]: string;
+}
