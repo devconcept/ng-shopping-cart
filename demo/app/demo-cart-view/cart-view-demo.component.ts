bdc4033d219fa9eb225a1d26c20184937171eb68
devconcept <devconcept@outlook.com>
2018-06-05 09:28:57 -0500

    Added tax and shipping to demo in cart-view

--- demo/app/demo-cart-view/cart-view-demo.component.ts
+++ demo/app/demo-cart-view/cart-view-demo.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { CartViewDisplay } from '../../../src/types';
+import {Component} from '@angular/core';
+import {CartViewDisplay} from '../../../src/types';
+import {CartService} from '../../../src/classes/cart.service';
+import {DemoCartItem} from '../demo-cart-item';
 
 @Component({
   selector: 'demo-cart-view',
@@ -8,9 +10,27 @@ import { CartViewDisplay } from '../../../src/types';
 })
 export class CartViewDemoComponent {
   display: CartViewDisplay = 'responsive-table';
+  tax = 0;
+  shipping = 0;
   showImages = true;
   useCustom = false;
 
   settingsCollapsed = false;
   resultsCollapsed = false;
+
+  constructor(public cartService: CartService<DemoCartItem>) {
+
+  }
+
+  setTaxRate() {
+    const val = parseFloat(this.tax.toString());
+    this.cartService.setTaxRate(val);
+    this.tax = this.cartService.getTaxRate();
+  }
+
+  setShipping() {
+    const val = parseFloat(this.shipping.toString());
+    this.cartService.setShipping(val);
+    this.shipping = this.cartService.getShipping();
+  }
 }
